3e4b8bba20fc77d4df249a7edf7fb14c310741fd
6504062610242salankosol <s6504062610242@email.kmutnb.ac.th>
2026-03-04 14:41:49 +0700

    Create Commit>> lab10no5.c

---
+++ Commit>> lab10no5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+struct Books
+{
+    char BookTitle[20];
+    int BookID;
+};
+
+void DisplayData(struct Books *CBook);
+
+int main() 
+{
+    struct Books ComputerBook;
+
+    printf("Enter Book ID : ");
+    scanf("%d",&ComputerBook.BookID);
+
+    printf("Enter Book Title : ");
+    scanf("%s",ComputerBook.BookTitle);
+
+    
+    DisplayData(&ComputerBook); 
+
+    return 0;
+}
+
+void DisplayData(struct Books *CBook)
+{
+  
+    printf("Book ID : %d\n", (*CBook).BookID);
+
+    printf("Book Title : %s\n", CBook->BookTitle);
+}
+
+/*input book id 101*/
+/*input book title c++*/
+
+/*output book id 101 */
+/*output book title c++*/
